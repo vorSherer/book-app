737b996e6e19851947849c90691ad78d28cdddea
Jordan Fox <jordanmichaelfox@hotmail.com>
2020-02-06 18:31:37 -0800

    pushing for another person to take over

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 'use strict';
 
+// App dependecies
 const express = require('express');
 const superagent = require('superagent');
 
+// App Setup
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+//app middleware
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+app.get('/search', (req, res) => {
+  res.status(200).send('You did a GET!');
+});
+
+app.listen(3000, () => console.log('Jordan was HERE'));
